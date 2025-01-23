de23001dcec99ec044d7ae3074dd4d966ebf9864
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2025-01-23 19:38:21 +0100

    Version 1.1.1

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import sys
 import os
 
+# Check arguments
+if len(sys.argv) != 3:
+    print("Usage: python main.py <project_path> <assets_path>")
+    sys.exit(1)
+
 # Markdown folder path
 project_path = sys.argv[1]
-project_path = os.path.realpath(project_path)
-markdown_file_list = []
-for root, dirs, files in os.walk(project_path):
-    for file in files:
-        if file.endswith(".md"):
-            markdown_file_list.append(os.path.join(root, file))
+try:
+    project_path = os.path.realpath(project_path)
+    markdown_file_list = []
+    for root, dirs, files in os.walk(project_path):
+        for file in files:
+            if file.endswith(".md"):
+                markdown_file_list.append(os.path.join(root, file))
+except Exception as e:
+    print("Error: " + str(e))
+    sys.exit(1)
 
 # Image folder path
 assets_path = sys.argv[2]
-assets_path = os.path.realpath(assets_path)
+try:
+    assets_path = os.path.realpath(assets_path)
+except Exception as e:
+    print("Error: " + str(e))
+    sys.exit(1)
 
 if __name__ == "__main__":
 
     # Check existence
     if not os.path.exists(project_path):
-        print("Project path " + project_path + " not found.")
+        print("Error: Project path " + project_path + " not found.")
         sys.exit(1)
     if not os.path.exists(assets_path):
-        print("Assets path " + assets_path + " not found.")
+        print("Error: Assets path " + assets_path + " not found.")
         sys.exit(1)
 
     print("Project path: " + project_path)
@@ -55,10 +68,10 @@ if __name__ == "__main__":
     # Decide to proceed or not
     if len(unused_images) == 0:
         print("All images are used.")
+        sys.exit(0)
     else:
         print("Unused images:")
-        for image in unused_images:
-            print(image)
+        print(unused_images)
     proceed = ""
     proceed = input("Do you want to delete these images? (y/n): ")
 
